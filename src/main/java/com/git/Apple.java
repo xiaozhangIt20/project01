b13f4a4a8b6af98f9228d6619631d34e49b9634f
zyc_glb <zycglb_pro@168.com>
2020-08-05 11:03:03 +0800

    推送到分支hot_fix

--- src/main/java/com/git/Apple.java
+++ src/main/java/com/git/Apple.java
@@ -5,6 +5,8 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("测试冲突....project01");
 		System.out.println("测试冲突....TestGit");
+		
+		System.out.println("测试创建分支hot_fix并修改文件....TestGit");
 	}
 
 }
